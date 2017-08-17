d28594c5f346cbd61c2a78903958863fd087f1ba
risa2 <blazek.risa@gmail.com>
2017-08-17 08:01:27 +0200

    func.h: BestPair function

--- func.h
+++ func.h
@@ -5,7 +5,7 @@
 namespace func
 {
 ///Pretypovani na r-hodnotu
-template<typename T>inline
+template<typename T>inline constexpr
 T&& Move(const T& obj)
 {
     return (T&&)obj;
@@ -31,42 +31,42 @@ size_t ArraySize(T& x)
 	return x.size();
 }
 
-template<typename T, typename T2>inline
+template<typename T, typename T2>inline constexpr
 const T& Max(const T& arg1, const T2& arg2)
 {
     return (arg1>arg2)?arg1:arg2;
 }
 
-template<typename T, typename T2, typename... types>inline
+template<typename T, typename T2, typename... types>inline constexpr
 const T& Max(const T& arg1, const T2& arg2, const types&... args)
 {
     return Max(Max(arg1, arg2), args...);
 }
 
-template<typename T, typename T2>inline
+template<typename T, typename T2>inline constexpr
 const T& Min(const T& arg1, const T2& arg2)
 {
     return (arg1<arg2)?arg1:arg2;
 }
 
-template<typename T, typename T2, typename... types>inline
+template<typename T, typename T2, typename... types>inline constexpr
 const T& Min(const T& arg1, const T2& arg2, const types&... args)
 {
     return Min(Min(arg1, arg2), args...);
 }
 
-template<typename T>
+template<typename T>inline constexpr
 T Limit(T x, T min, T max)
 {
     return func::Min(func::Max(x, min), max);
 }
-template<typename T, typename T2>inline
+template<typename T, typename T2>inline constexpr
 bool Ordered(const T& arg1, const T2& arg2)
 {
     return arg1<arg2;
 }
 
-template<typename T, typename T2, typename... types>inline
+template<typename T, typename T2, typename... types>inline constexpr
 bool Ordered(const T& arg1, const T2& arg2, const types&... args)
 {
     return Ordered(arg1, arg2)&&Ordered(arg2, args...);
@@ -87,5 +87,20 @@ template<typename... T>inline
 auto Avg(const T&... arg)
 {
     return Sum(arg...)/sizeof...(arg);
+}
+
+
+template<typename Pair, typename ContA, typename ContB, typename Func>
+Pair BestPair(const ContA& list_a, const ContB& list_b, Func better)
+{
+    Pair best{list_a.begin(), list_b.begin()};
+    for(auto& a:list_a)
+    {
+        for(auto& b:list_b)
+        {
+            best=better(best, {&a, &b});
+        }
+    }
+    return best;
 }
 }
