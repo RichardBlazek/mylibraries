378eb591a33d2f50998ace31b7f9d43b57ea20c8
risa2 <blazek.risa@gmail.com>
2017-10-01 20:08:32 +0200

    func.h: 1,2,2,3 is Ordered

--- func.h
+++ func.h
@@ -21,36 +21,36 @@ void Swap(T& a, T& b)
 }
 
 template<typename T, size_t S>constexpr
-size_t ArraySize(T(&)[S])
+size_t Size(T(&)[S])
 {
 	return S;
 }
 template<typename T>constexpr
-size_t ArraySize(T& x)
+size_t Size(T& x)
 {
 	return x.size();
 }
 
 template<typename T, typename T2>inline constexpr
-const T& Max(const T& arg1, const T2& arg2)
+T Max(const T& arg1, const T2& arg2)
 {
     return (arg1>arg2)?arg1:arg2;
 }
 
 template<typename T, typename T2, typename... types>inline constexpr
-const T& Max(const T& arg1, const T2& arg2, const types&... args)
+T Max(const T& arg1, const T2& arg2, const types&... args)
 {
     return Max(Max(arg1, arg2), args...);
 }
 
 template<typename T, typename T2>inline constexpr
-const T& Min(const T& arg1, const T2& arg2)
+T Min(const T& arg1, const T2& arg2)
 {
     return (arg1<arg2)?arg1:arg2;
 }
 
 template<typename T, typename T2, typename... types>inline constexpr
-const T& Min(const T& arg1, const T2& arg2, const types&... args)
+T Min(const T& arg1, const T2& arg2, const types&... args)
 {
     return Min(Min(arg1, arg2), args...);
 }
@@ -71,26 +71,26 @@ bool Ordered(const T& arg1, const T2& arg2, const types&... args)
 {
     return Ordered(arg1, arg2)&&Ordered(arg2, args...);
 }
-template<typename T, typename T2>inline
+template<typename T, typename T2>inline constexpr
 auto Sum(const T& arg1, const T2& arg2)
 {
     return arg1+arg2;
 }
 
-template<typename T, typename... Types>inline
+template<typename T, typename... Types>inline constexpr
 auto Sum(const T& arg1, const Types&... args)
 {
     return arg1+Sum<Types...>(args...);
 }
 
-template<typename... T>inline
+template<typename... T>inline constexpr
 auto Avg(const T&... arg)
 {
     return Sum<T...>(arg...)/sizeof...(arg);
 }
 
 
-template<typename Pair, typename ContA, typename ContB, typename Func>
+template<typename Pair, typename ContA, typename ContB, typename Func>constexpr
 Pair BestPair(const ContA& list_a, const ContB& list_b, Func better)
 {
     Pair best{list_a.begin(), list_b.begin()};
